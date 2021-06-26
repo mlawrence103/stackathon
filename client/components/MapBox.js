1154ceb13a074f7289097ac6c04327ddbeff8e22
Mabel Lawrence <mabelmail3@gmail.com>
2021-06-25 21:26:00 -0400

    pop ups on markers

--- client/components/MapBox.js
+++ client/components/MapBox.js
@@ -46,11 +46,40 @@ class MapBox extends React.PureComponent {
     const y2 = coordinates[1][1];
     const midX = (x1 + x2) / 2;
     const midY = (y1 + y2) / 2;
+    const westmost = Math.min(x1, x2) - 0.005;
+    const eastmost = Math.max(x1, x2) + 0.005;
+    const southmost = Math.min(y1, y2) - 0.005;
+    const northmost = Math.max(y1, y2) + 0.005;
     // console.log(`middle (${midX},${midY})`);
     this.setState({ ...this.state, midLng: midX, midLat: midY });
-    await new mapboxgl.Marker().setLngLat([midX, midY]).addTo(this.state.map);
-    await new mapboxgl.Marker().setLngLat([x1, y1]).addTo(this.state.map);
-    await new mapboxgl.Marker().setLngLat([x2, y2]).addTo(this.state.map);
+    await new mapboxgl.Marker()
+      .setLngLat([midX, midY])
+      .addTo(this.state.map)
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+          '<h3>' + 'Meeting Point' + '</h3>'
+        )
+      );
+    await new mapboxgl.Marker()
+      .setLngLat([x1, y1])
+      .addTo(this.state.map)
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+          '<h3>' + 'Address 1' + '</h3><p>' + this.state.address1 + '</p'
+        )
+      );
+    await new mapboxgl.Marker()
+      .setLngLat([x2, y2])
+      .addTo(this.state.map)
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+          '<h3>' + 'Address2' + '</h3><p>' + this.state.address2 + '</p'
+        )
+      );
+    this.state.map.fitBounds([
+      [westmost, southmost],
+      [eastmost, northmost],
+    ]);
   }
 
   async componentDidMount() {
