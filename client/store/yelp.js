cb9a21114e03e68418215a72b9bef08bafbf9412
Mabel Lawrence <mabelmail3@gmail.com>
2021-06-27 23:46:12 -0400

    midway through adding events feature

--- client/store/yelp.js
+++ client/store/yelp.js
@@ -23,9 +23,8 @@ export const findFood = (long, lat) => {
       console.log('IN FIND FOOD with coords: ', lat, long);
       const keyRes = await axios.get('/api/yelpApiKey');
       const key = keyRes.data;
-      console.log('found yelp api key in thunk: ', key);
       const results = await axios.get(
-        `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${long}&categories=restaurants&limit=10&radius=402`,
+        `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${long}&categories=restaurants&limit=10&sort_by=rating&radius=402`,
         {
           headers: {
             Authorization: `Bearer ${key}`,
@@ -34,6 +33,28 @@ export const findFood = (long, lat) => {
       );
       console.log('results from find food thunk: ', results.data.businesses);
       return dispatch(_findFood(results.data.businesses));
+    } catch (error) {
+      console.log("Couldn't find lunch spots", error);
+    }
+  };
+};
+
+export const findEvents = (long, lat) => {
+  return async (dispatch) => {
+    try {
+      console.log('IN FIND EVENTS thunk with coords: ', lat, long);
+      const keyRes = await axios.get('/api/yelpApiKey');
+      const key = keyRes.data;
+      const results = await axios.get(
+        `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/events/search?latitude=${lat}&longitude=${long}&categories=sightseeing,culture,arts&sort_by=rating&radius=800`,
+        {
+          headers: {
+            Authorization: `Bearer ${key}`,
+          },
+        }
+      );
+      console.log('results from find food thunk: ', results.data.businesses);
+      return dispatch(_findEvents(results.data.businesses));
     } catch (error) {
       console.log("Couldn't find lunch spots", error);
     }
